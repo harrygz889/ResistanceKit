d4d340b4964c98a23d1ab347fe5efd113ab39d11
Harry Zumwalt <harry.zum@gmail.com>
2017-03-14 18:59:43 -0500

    added avatar helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def avatar_for(user, options = {size: 200})
+    size = options[:size]
+    if !user.avatar.file.nil?
+      image_tag user.avatar.url(:display), size: size
+    else
+      image_tag 'default.png', size: size
+    end
+  end
 end
